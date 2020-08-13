a2a8d344741a7f8af4f8bd8aa0822d9f9348aa4f
YWHSDF <39758029+YWHSDF@users.noreply.github.com>
2020-08-13 12:28:46 +0800

    Update index.py

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import requests
 
-auth_refresh_url = ''
+auth_refresh_url = 'https://access.video.qq.com/user/auth_refresh?vappid=11059694&vsecret=fdf61a6be0aad57132bc5cdf78ac30145b6cd2c1470b0cfe&type=qq&g_tk=&g_vstk=1182418585&g_actk=1223137314&callback=jQuery1910646484216808564_1597292730064&_=1597292730065'
 sckey = ''
 
 ftqq_url = "https://sc.ftqq.com/%s.send"%(sckey)
@@ -21,7 +21,7 @@ url2 = 'https://v.qq.com/x/bu/mobile_checkin'
 
 login_headers = {
     'Referer': 'https://v.qq.com',
-    'Cookie': 'tvfe_boss_uuid=********; pgv_pvid=********; video_guid=***********; video_platform=2; pgv_info=ssid=***********; pgv_pvi=*************; pgv_si=*************; _qpsvr_localtk=***************; ptisp=; ptui_loginuin=************; RK=*************; ptcz=***************; main_login=qq; vqq_access_token=****************; vqq_appid=101483052; vqq_openid=********************; vqq_vuserid=*********************; vqq_vusession=dzsfo; vqq_refresh_token=*****************; uid=**************;'
+    'Cookie': 'pgv_pvi=8591048704; pgv_si=s4709841920; _qpsvr_localtk=0.2544835028399788; RK=teTpb83HaO; ptcz=5f1c02886be0876099b4c2740b30b7ae6f5231a1d225e35fbab4b727c800d3ef; tvfe_boss_uuid=5a7115e7ddd82c12; video_guid=e1312e1fbdcf33de; video_platform=2; pgv_pvid=9591014368; pgv_info=ssid=s6424935120; ptui_loginuin=918914492; main_login=qq; vqq_access_token=0F0AA7470FD45A68DDA6ED702D7B61F6; vqq_appid=101483052; vqq_openid=704571A2210013B25D5C185413B845BA; vqq_vuserid=530398448; vqq_vusession=6bH7CKJa7xXacdeSNANiTg..; vqq_refresh_token=63B124428E4D2F479A53B7D31BA9C918; vqq_next_refresh_time=6600; vqq_login_time_init=1597292729; login_time_init=2020-8-13 12:25:29'
 }
 
 login = requests.get(auth_refresh_url, headers=login_headers)
